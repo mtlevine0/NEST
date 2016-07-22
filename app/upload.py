54847dd5a53092df4d0cb16f1844c310ac640837
Matt Levine <mtlevine0@gmail.com>
2016-07-22 05:53:43 +0000

    adding prop files.

--- app/upload.py
+++ app/upload.py
@@ -2,8 +2,32 @@ from flask import jsonify, Blueprint, request, Response
 import database
 import properties
 import json
+import smtplib
 
 upload_api = Blueprint('upload_api', __name__)
+
+def send_email(user, pwd, recipient, subject, body):
+    gmail_user = user
+    gmail_pwd = pwd
+    FROM = user
+    TO = recipient if type(recipient) is list else [recipient]
+    SUBJECT = subject
+    TEXT = body
+
+    # Prepare actual message
+    message = """From: %s\nTo: %s\nSubject: %s\n\n%s
+    """ % (FROM, ", ".join(TO), SUBJECT, TEXT)
+    try:
+        server = smtplib.SMTP("smtp.gmail.com", 587)
+        server.ehlo()
+        server.starttls()
+        smtpserver.ehlo()
+        server.login(gmail_user, gmail_pwd)
+        server.sendmail(FROM, TO, message)
+        server.close()
+        print 'successfully sent the mail'
+    except:
+        print "failed to send mail"
 
 @upload_api.route('/', methods=['GET'])
 def upload():
@@ -14,16 +38,18 @@ def publish():
     contentLength = int(request.headers.get('content-length'))
     body = request.data
     #validate json
+    email = json.loads(body)['email']
     print contentLength, properties.maxFileSize
     if contentLength < properties.maxFileSize:
         #Return a 200 OK, Make a db entry
         doc = json.loads(body)
         try:
-            database.db.create(doc)
-            print doc
+            entry = database.db.create(doc)
+            print entry
         except:
             statusCode = 500
         statusCode = 200
+        send_email(properties.gmailAccount, properties.gmailPassword, email, properties.emailUploadSubject, "new paste has been created")
     elif contentLength > properties.maxFileSize:
         #Return a 413 Payload to large, move on
         statusCode = 413
--- database.py
+++ database.py
@@ -9,3 +9,18 @@ couch.resource.credentials = (properties.dbConnectUser, properties.dbConnectPass
 
 # Connect to DB to retrieve list of expired Eggs
 db = couch[properties.dbConnectName]
+
+def getDBEntry(uid):
+    doc = 0
+    for id in database.db:
+        if id == uid:
+            doc = database.db.get(id)
+    return doc
+
+
+def updateDBEntry(doc):
+    return database.db.save(doc)
+    
+    
+def deleteDBEntry(doc):
+    return database.db.delete(doc)
