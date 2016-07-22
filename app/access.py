ad0dd0dafbc19702c1e27a76984bc9d56a1a5d6b
Matt Levine <mtlevine0@gmail.com>
2016-07-22 01:17:52 +0000

    Added flash debugging, database connectin and query.

--- app/access.py
+++ app/access.py
@@ -1,8 +1,13 @@
 from flask import jsonify, Blueprint
 import couchdb
+import database
 
 access_api = Blueprint('access_api', __name__)
 
 @access_api.route('/<uid>', methods=['GET'])
 def fetch(uid):
-    return uid
+    data = {}
+    for i, id in enumerate(database.db):
+        print i, id
+        data[i] = id
+    return jsonify(data)
