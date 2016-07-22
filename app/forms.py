a7819059adee9ba4df88e9928b7a669a9b647ad3
Zach Ison <ison031@gmail.com>
2016-07-22 08:40:44 +0000

    forms.py updates

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import Form
-from wtforms import StringField
+from wtforms import StringField, HiddenField
 from wtforms.validators import DataRequired
 
 class AuthForm(Form):
     pass_input = StringField('pass_input', validators=[DataRequired()])
+    uid = StringField('uid_input', validators=[DataRequired()])
