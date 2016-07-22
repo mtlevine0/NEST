4a17559fb2a0b63ed9e1804f8927f1654cbd7801
Joe Castle <brocatles@gmail.com>
2016-07-22 08:29:48 +0000

    Adding final delete.py

--- app/delete.py
+++ app/delete.py
@@ -5,19 +5,24 @@ import objectstorage
 delete_api = Blueprint('delete_api', __name__)
 
 #API to remove the page using the uid and admin password
-@delete_api.route('/delete/<uid>/<password>', methods=['POST'])
+@delete_api.route('/delete/<uid>/<urlAdminPassword>', methods=['POST'])
 def removeEgg(uid, urlAdminPassword):
     
     #Retrieve Doc
     doc = database.getDBEntry(uid)
+    if (doc == 0):
+        result = render_template('error.html',message="404 Page Not Found - invalid Doc uid")
+        return result
     
     #Set Variables From Doc
     try:
         docFileName = str(doc['filename'])
         docAdminPassword = str(doc['admin_password'])
-        docType = docTypeCheck(doc)
+        docType = docTypeCheck(doc, uid)
     except:
         print "There was an issue setting up the variables for Document ID: %s" % uid
+        result = render_template('error.html',message="500 Internal Server Error - Variables not defined")
+        return result
     
     if (docAdminPassword != urlAdminPassword):
         result = render_template('error.html',message="403 Access Forbidden")
@@ -25,10 +30,10 @@ def removeEgg(uid, urlAdminPassword):
     elif (docAdminPassword == urlAdminPassword):
         try:
             if (docFileName > "" or docType == "file"):
-                fileDeleteSuccess = deleteOSFile(docFileName)
-                result =  render_template('error.html',message=deleteDBDoc(doc,fileDeleteSuccess))
+                fileDeleteSuccess = deleteOSFile(docFileName,uid)
+                result =  render_template('error.html',message=deleteDBDoc(doc,fileDeleteSuccess,uid))
             else:
-                result = render_template('error.html',message=deleteDBDoc(doc,True))
+                result = render_template('error.html',message=deleteDBDoc(doc,True,uid))
         except:
             result = render_template('error.html',message="500 Internal Server Error - A delete failed somplace")
             print "There was an exception processing the deletion of the Invalid Type Document ID: %s" % uid
@@ -36,13 +41,14 @@ def removeEgg(uid, urlAdminPassword):
     return result
     
 
-def docTypeCheck(document):
-    print "docTypeCheck for ID: %s" % id
+def docTypeCheck(document, uid):
+    print "docTypeCheck for ID: %s" % uid
+    
     #Try to set the docType Variable
     try:
         docType = document['type'].lower()
     except:
-        print "An exception occurred checking the docType for Document ID: %s" % id
+        print "An exception occurred checking the docType for Document ID: %s" % uid
         return "invalid"
         
     if (docType == "file"):
@@ -53,23 +59,23 @@ def docTypeCheck(document):
         return "invalid"
 
 #Delete Document from the DB
-def deleteDBDoc(document,FileRemovalStatus):
+def deleteDBDoc(document,FileRemovalStatus,uid):
     if(FileRemovalStatus == True):
         try:
             database.db.delete(document)
-            print "We have successfully deleted document with ID: %s" % id
+            print "We have successfully deleted document with ID: %s" % uid
             return "Delete Successful"
         except:
-            print "Something went wrong trying to delete the DB document associated with document ID: %s" % id
+            print "Something went wrong trying to delete the DB document associated with document ID: %s" % uid
             return "500 Internal Server Error - DB Delete Failed"
     else:
         return "500 Internal Server Error - Potential File Delete Failed"
         
-def deleteOSFile(fileName):
+def deleteOSFile(fileName,uid):
     try:
         objectstorage.deleteFile(fileName)
-        print "We have successfully deleted the file associated with document ID: %s" % id
+        print "We have successfully deleted the file associated with document ID: %s" % uid
         return True
     except:
-        print "Something went wrong trying to delete the File associated with document ID: %s" % id
+        print "Something went wrong trying to delete the File associated with document ID: %s" % uid
         return False
