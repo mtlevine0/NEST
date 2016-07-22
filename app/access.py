6dafa6a9f76aa23a3b39deafec57a5e1de455691
Zach Ison <ison031@gmail.com>
2016-07-22 01:20:02 +0000

    Prep for pull

--- app/access.py
+++ app/access.py
@@ -1,7 +1,32 @@
 from flask import jsonify, Blueprint
+import couchdb
 
 access_api = Blueprint('access_api', __name__)
 
 @access_api.route('/<uid>', methods=['GET'])
 def fetch(uid):
+    
+    #TODO: if uid in db
+    
+        #TODO: pull back data
+    
+        #TODO: if password != NULL
+        
+            #TODO: return autorizaion page
+        
+        #TODO: if type == file
+            
+            #TODO: Grab the file
+    
+    #TODO: Increment self-destruct counter 
+    
+    #TODO: populate the template 
+    
     return 'access page'
+    
+    
+@access_api.route('/<uid>/<auth>', methods=['POST'])
+def authorization(uid, auth):
+    
+
+    return 'authorization page'
--- app/upload.py
+++ app/upload.py
@@ -2,6 +2,6 @@ from flask import jsonify, Blueprint
 
 upload_api = Blueprint('upload_api', __name__)
 
-@upload_api.route('/publish', methods=['POST'])
+@upload_api.route('/', methods=['POST', 'GET'])
 def upload():
     return 'upload page'
