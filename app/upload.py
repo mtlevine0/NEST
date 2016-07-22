b0bc68b285ac3a85a696671c8b8c3e9909218b68
Matt Levine <mtlevine0@gmail.com>
2016-07-22 00:20:05 +0000

    updated couchdb dependency in requirements.txt.

--- app/upload.py
+++ app/upload.py
@@ -2,6 +2,6 @@ from flask import jsonify, Blueprint
 
 upload_api = Blueprint('upload_api', __name__)
 
-@upload_api.route('/', methods=['POST'])
+@upload_api.route('/publish', methods=['POST'])
 def upload():
     return 'upload page'
