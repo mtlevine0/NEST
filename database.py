2d76f78fee3ecef3567f3d23543a0d2e057a135e
Matt Levine <mtlevine0@gmail.com>
2016-07-22 01:27:41 +0000

    Cleaned up database.py.

--- database.py
+++ database.py
@@ -10,14 +10,9 @@ dbConnectName = data["dbName"]
 dbConnectUser = data["dbUser"]
 dbConnectPassword = data["dbPassword"]
 
-
 # Set DB Connection
 couch = couchdb.Server("https://%s.cloudant.com" % dbConnectUser)
 couch.resource.credentials = (dbConnectUser, dbConnectPassword)
 
-
 # Connect to DB to retrieve list of expired Eggs
 db = couch[dbConnectName]
-
-
-print db.info()
